70382bd30b933b4e2a3b6a4957e34b8377a02fff
nhatvinh0110 <62092384+nhatvinh0110@users.noreply.github.com>
2021-12-15 20:00:17 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,84 @@
 from flask import Flask
-from flask import request,url_for
+from flask import request, url_for
 from flask import render_template
-from flask import redirect,Response
+from flask import redirect, Response
+import glob
 import sqlite3
+from sqlite3 import Error
 import json
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
-@app.route('/<img_path>', methods=['POST', 'GET'])
-def mainpage(img_path=None):
+@app.route('/<page>', methods=['POST', 'GET'])
+def mainpage(page=None):
+    detects = select_detects()
     if request.method == 'GET':
-        if img_path:
-            return render_template('draw.html')
+        if page:
+            if str(page).isdigit():
+                page = int(page)
+                if page*10 < len(detects):
+                    return render_template('index.html', imgs=detects[(page-1)*10:page*10],max_lenght = len(detects),cur_page= page)
+                else:
+                    if page*10 > len(detects)+10 :
+                        return render_template('index.html', imgs=detects[0:10],max_lenght = len(detects),cur_page= page)
+                    return render_template('index.html', imgs=detects[(page-1)*10:],max_lenght = len(detects),cur_page= page)
+            else:
+                return render_template('index.html', imgs=detects[0:10],max_lenght = len(detects),cur_page= page)
         else:
-            return render_template('draw.html')
-	
-	
+            return render_template('index.html', imgs=detects[0:10],max_lenght = len(detects),cur_page= page)        
     if request.method == 'POST':
-        print("p1: " + request.form['p1']);
+        jObject = json.loads(request.form['data'])
+        detects_pos = jObject['detects']
+        update_detects(detects_pos)
         resp = Response("saved")
         return resp
+
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def select_detects():
+    database = "./database.db"
+    conn = create_connection(database)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Detect")
+    Detects = cur.fetchall()
+    conn.commit
+    return Detects
+
+
+def update_detects(up_detects):
+    database = "./database.db"
+    conn = create_connection(database)
+    sql = ''' UPDATE Detect
+              SET Loaixe = ? ,
+                  Path = ? ,
+                  x1 = ?,
+                  y1 = ?,
+                  x2 = ?,
+                  y2 = ?,
+                  x3 = ?,
+                  y3 = ?,
+                  x4 = ?,
+                  y4 = ?,
+                  TrangThai = ?
+              WHERE ID = ?'''
+    cur = conn.cursor()
+    for detect in up_detects:
+        conn.execute(sql, (detect[1], detect[2], detect[3], detect[4], detect[5],
+                     detect[6], detect[7], detect[8], detect[9], detect[10], detect[11], detect[0]))
+        conn.commit()
+
 
 if __name__ == '__main__':
     app.debug = True
---
+++ init_data.py
@@ -0,0 +1,37 @@
+import glob
+import os
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+def create_detect(conn, Detect):
+    sql = ''' INSERT INTO Detect(LoaiXe,Path,TrangThai)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, Detect)
+    conn.commit()
+    return cur.lastrowid
+def select_detects(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Detect")
+    Detects = cur.fetchall()
+    return Detects
+if __name__ == '__main__':
+    database = "./database.db"
+    con = create_connection(database)
+    with con:
+        # for img in glob.glob("./vehicle/moto/*.jpg"):
+        #     print(os.path.basename(img))
+        #     Detect = ('moto',os.path.basename(img),'null')
+        #     Detect_id = create_detect(con,Detect)
+        detects = select_detects(con)
+        for detect in detects:
+            print(detect[0])
+            
